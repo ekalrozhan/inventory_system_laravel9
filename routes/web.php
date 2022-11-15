e4eff8915d18aaa9a8056260c181bb58b671b42d
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-11-15 22:34:01 +0800

    Product edit form view, updated

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,8 @@ Route::controller(ProductController::class)->group(function () {
     Route::get('/product/all', 'ProductAll')->name('product.all');
     Route::get('/product/add', 'ProductAdd')->name('product.add');
     Route::post('/product/store', 'ProductStore')->name('product.store');
+    Route::get('/product/edit/{id}', 'ProductEdit')->name('product.edit');
+    Route::post('/product/update', 'ProductUpdate')->name('product.update');
 
 
  
